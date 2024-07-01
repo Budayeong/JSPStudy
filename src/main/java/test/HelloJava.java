a572d9b838f1f12372315eadf4c087beedf56173
dayeong <dyboo1346@naver.com>
2024-07-01 18:00:46 +0900

    jsp github 연동

---
+++ src/main/java/test/HelloJava.java
@@ -0,0 +1,18 @@
+package test;
+
+public class HelloJava {
+	
+	public static int myFn() {
+		int sum =0;
+		for(int i=1 ; i<=10 ; i++) {
+			sum+=i;
+		}
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello world~");
+	}
+
+}
